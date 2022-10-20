91d129f9de02d8ecfffc086003036287b86ad8f2
Pedro Siqueira das Neves <pedro.siqueira@poli.ufrj.br>
2022-10-20 18:20:37 -0300

    Add functions to code

--- board_detector.py
+++ board_detector.py
@@ -1,45 +1,41 @@
 import cv2
 
-# path
-path = r'C:\Users\T-Gamer\Documents\Faculdade\Telecom\Images\test_board3.png'
 
-# Using cv2.imread() method
-img = cv2.imread(path)
+def detect_edges(path):
 
-# Converting image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+        # Using cv2.imread() method
+        img = cv2.imread(path)
 
-# Applying GaussianBlur
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        # Converting image to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 
-# Applying Canny Edge detection
-canny = cv2.Canny(blur, 50, 150)
+        # Applying GaussianBlur
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-# Finding contours
+        # Applying Canny Edge detection
+        canny = cv2.Canny(blur, 50, 150)
 
-contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # Finding contours
 
-# Finding the biggest contour
-biggest = None
-max_area = 0
-for i in contours:
-        area = cv2.contourArea(i)
-        if area > 100:
-                peri = cv2.arcLength(i,True)
-                approx = cv2.approxPolyDP(i,0.02*peri,True)
-                if area > max_area and len(approx)==4:
-                        biggest = approx
-                        max_area = area
+        contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-#cv2.imshow('Pre Contorno', img)
-#cv2.waitKey()
-# Draw the biggest contour
-cv2.drawContours(img, [biggest], -1, (0,255,0), 3)
+        # Finding the biggest contour
+        biggest = None
+        max_area = 0
+        for i in contours:
+                area = cv2.contourArea(i)
+                if area > 100:
+                        peri = cv2.arcLength(i,True)
+                        approx = cv2.approxPolyDP(i,0.02*peri,True)
+                        if area > max_area and len(approx)==4:
+                                biggest = approx
+                                max_area = area
 
-# Displaying the image
-#cv2.imshow('image', img)
-#cv2.imshow('gray', gray)
-#cv2.imshow('blur', blur)
-cv2.imwrite('te.png', img)
-cv2.imshow('\Images\test4.png', img)
-cv2.waitKey()
+        # Draw the biggest contour
+        # cv2.drawContours(img, [biggest], -1, (0,255,0), 3)
+
+        # Displaying the image with contour
+        #cv2.imwrite('te.png', img)
+        #cv2.imshow('\Images\test4.png', img)
+        #cv2.waitKey()
+        return [biggest[0][0], biggest[2][0]]
--- detect_voice.py
+++ detect_voice.py
@@ -1,13 +1,17 @@
 import speech_recognition
 
-recognizer = speech_recognition.Recognizer()
-with speech_recognition.Microphone() as src:
-    try:
-        audio = recognizer.adjust_for_ambient_noise(src)
-        print("Threshold Value After calibration:" + str(recognizer.energy_threshold))
-        print("Please speak:")
-        audio = recognizer.listen(src)
-        speech_to_txt = recognizer.recognize_google(audio_data = audio, language ="pt-BR").lower()
-        print(speech_to_txt)
-    except Exception as ex:
-        print("Sorry. Could not understand.")
+
+def recognize_voice():
+    recognizer = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as src:
+        try:
+            audio = recognizer.adjust_for_ambient_noise(src)
+            print("Threshold Value After calibration:" + str(recognizer.energy_threshold))
+            print("Please speak:")
+            audio = recognizer.listen(src)
+            speech_to_txt = recognizer.recognize_google(audio_data = audio, language ="pt-BR").lower()
+        except Exception as ex:
+            print("Sorry. Could not understand.")
+    return speech_to_txt
+
+print(recognize_voice())
